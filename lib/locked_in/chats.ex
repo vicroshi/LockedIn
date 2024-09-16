fd075d409baafae1c9fdf49d0305ef4d19d19953
vicroshi <lambrostars@gmail.com>
2024-09-17 00:50:05 +0300

    chats index fix

--- lib/locked_in/chats.ex
+++ lib/locked_in/chats.ex
@@ -17,7 +17,7 @@ defmodule LockedIn.Chats do
       preload: [:latest_message, :user1, :user2],
       where: c.user1_id == ^user.id or c.user2_id == ^user.id,
       order_by: [desc: m.inserted_at ],
-      grqoup_by: c.id,
+      group_by: c.id,
       select: c
     )
     |> IO.inspect()
