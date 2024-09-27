060c10c9697f2683fdf94a939af95ac7445fe18c
vicroshi <lambrostars@gmail.com>
2024-09-27 21:26:16 +0300

    applicant

--- lib/locked_in_web/controllers/job_controller.ex
+++ lib/locked_in_web/controllers/job_controller.ex
@@ -12,7 +12,7 @@ defmodule LockedInWeb.JobController do
   end
 
   def mark_viewed(conn, %{"job_id" => id}) do
-    job = Jobs.get_job!(id) |> with_assoc([:user, :skills, :applications])
+    job = Jobs.get_job!(id) |> with_assoc([:user, :skills, [applications: :applicant]])
     case Jobs.mark_viewed(job.id,conn.assigns.current_user.id) do
       {1, _} -> conn
                 |> put_status(:created)
